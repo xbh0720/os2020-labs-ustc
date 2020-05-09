d33586a390bf7a8b9a5d4786fcc424bd31443fd5
xbh0720 <61641325+xbh0720@users.noreply.github.com>
2020-05-09 17:10:29 +0800

    tick

---
+++ lab4/src/myOS/kernel/tick.c
@@ -0,0 +1,12 @@
+//周期性时钟中断
+extern void updateWallClock(void);
+extern int myPrintk(int color,const char *format, ...);
+int count=0;
+void tick(void)
+{
+	count++;	 //全局变量count在每次tick后+1
+//每tick100次几乎经过了1s，用于时钟维护
+	if(count==100){
+		updateWallClock();
+		count=0;}
+}
